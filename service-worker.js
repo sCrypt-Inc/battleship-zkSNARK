f55e78d8d20d61e3819e838babbe62bbd881a114
hh <hh@scrypt.io>
2022-09-03 23:23:15 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zk-battleship/precache-manifest.81147e2e265b1a358c286ffaaee89eeb.js"
+  "/zk-battleship/precache-manifest.48ee2fc1ca639adb6fdfdc582e9dafcd.js"
 );
 
 self.addEventListener('message', (event) => {
